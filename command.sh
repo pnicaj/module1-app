c153e12e9f0e7f0dc890685bbd87f0e72a6d50ef
pnicaj <31523156+pnicaj@users.noreply.github.com>
2022-03-15 16:02:09 -0400

    added command file

---
+++ command.sh
@@ -0,0 +1,12 @@
+# Repo_URL 
+
+npx create-react-app module1-app
+gh add .
+git commit -m 'First commit'
+git push -u origin HEAD
+git checkout -b update_logo
+git add . 
+git commit -m 'adding branch'
+git push -u origin HEAD
+gh pr create
+gh pr merge
